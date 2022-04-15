ea5834a112e3933cd211dbdb1ed7092805733cdb
William Nemencha <william@ethicdevs.com>
2022-04-15 10:47:42 +0200

    fix(useSelect): do not depend on initialState anymore but from getState

--- src/hooks/useSelect.ts
+++ src/hooks/useSelect.ts
@@ -8,10 +8,11 @@ import { useGlobalState } from "./useGlobalState";
 export function useSelect<S extends FluxBaseState, R extends unknown>(
   selector: (state: S) => R,
 ) {
-  const { state } = useGlobalState();
+  const { getState } = useGlobalState();
+  const state = getState();
   return useMemo(
     () => selector(state as S) as R,
     // magic bit here, use the return of the selector to prevent useless renders
-    [selector, state, selector(state as S)],
+    [selector, selector(state as S)],
   );
 }
